5370a7432e17a42280a4bf1626919f10fe4bd296
Pizzosta <pizzostor@gmail.com>
2023-05-27 05:58:25 -0700

    a Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress.

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "todos?userId={}".format(sys.argv[1])).json()
 
-    completed = [todo.get("title") for todo in todos 
+    completed = [todo.get("title") for todo in todos
                  if todo.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
