b08da1f1a4738eb69c932125dc331f011e159328
Pizzosta <pizzostor@gmail.com>
2023-06-08 17:31:24 -0700

    a recursive function that queries the Reddit API, parses the title of all hot articles, and prints a sorted count of given keywords

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+RedditAPI Module that prints the titles of hot articles
+and prints a sorted count of given keywords
+(case-insensitive, delimited by spaces. Javascript should count
+as javascript, but java should not)
+"""
+
+import requests
+
+
+def count_words(subreddit, word_list, after=None, count_dict=None):
+    """
+    Queries the Reddit API and prints the titles of hot articles
+    and prints a sorted count of given keywords
+    for a given subreddit recursively.
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=100&after={}'.format(
+        subreddit, after)
+    headers = {'User-Agent': 'Pizzosta'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        articles = data['data']['children']
+        if count_dict is None:
+            count_dict = {}
+
+        for article in articles:
+            title = article['data']['title'].lower()
+            for word in word_list:
+                if (word.lower() in title and
+                        not title.endswith(('.', '!', '_'))):
+                    if word in count_dict:
+                        count_dict[word] += 1
+                    else:
+                        count_dict[word] = 1
+
+        after = data['data']['after']
+        if after is not None:
+            return count_words(subreddit, word_list, after=after,
+                               count_dict=count_dict)
+        else:
+            sorted_counts = sorted(count_dict.items(),
+                                   key=lambda x: (-x[1], x[0]))
+            for word, count in sorted_counts:
+                print('{}: {}'.format(word, count))
+    else:
+        return None
