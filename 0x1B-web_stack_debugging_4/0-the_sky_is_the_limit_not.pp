7cce938ed97434725c96d880742db1311331ba55
Pizzosta <pizzostor@gmail.com>
2023-06-17 18:18:57 -0700

    puppet file to increase number of requests processed

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+exec {'replace':
+  provider => shell,
+  command  => "sudo sed -i 's/ULIMIT=\"-n 15\"/ULIMIT=\"-n 5000\"/' /etc/default/nginx",
+  before   => Exec['restart'],
+}
+
+exec {'restart':
+  provider => shell,
+  command => 'sudo service nginx restart',
+}
